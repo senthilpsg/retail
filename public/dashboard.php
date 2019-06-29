8b0dbcf7259822979f7e58a7808d5207cfe61997
shashwanth <perfectlyimperfect7688@gmail.com>
2019-06-29 13:25:39 +0530

    dashboard page

---
+++ public/dashboard.php
@@ -0,0 +1,59 @@
+<html>
+<head>
+
+<style>
+
+.left-col{
+    border-right:1px solid blue;
+
+    width:20%;
+
+    float:left;
+
+    height:100vh;
+    background: lightgray;
+}
+
+.right-col{
+    /* border:1px solid red; */
+    width:78%;
+    float:left;
+    height:100vh;
+    padding:10px;
+    padding-top:0;
+}
+
+.left-menu li{
+        padding:5px;
+        
+        list-style:none;
+}
+
+
+</style>
+
+</head>
+<body>
+<div class="container">
+    <div class="left-col">
+    
+    <ul class="left-menu">
+    <li> 
+    <a href="dashboard.php">
+    Dashboard
+    </a>
+    </li>
+    <li>Customers</li>
+    <li>Sales</li>
+    <li>Purchase</li>
+    <li>Inventory</li>
+    <li>Settings</li>
+    </ul>
+    </div>
+    <div class="right-col">
+    <h1>Dashboard</h1>
+    </div>
+</div>
+
+</body>
+</html>
