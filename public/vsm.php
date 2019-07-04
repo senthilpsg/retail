1c738458c67159394b02402ea9511d3b13450085
MONISH-VELUSAMY <monish.vgp@gmail.com>
2019-07-04 12:15:00 +0530

    sign up page

---
+++ public/vsm.php
@@ -0,0 +1,72 @@
+<html>
+<head>
+</head>
+<body> 
+<center>
+<img src="img/logo-full-black.svg" alt="wave">
+</center>
+<h1 style="text-align: center; font-family: Averta,sans-serif;">Check out Wave - it's free!</h1>
+<center>
+    <form>
+<p id="tsa"> Wave helps over 3.5 million freelancers, consultants,<br>
+and small business simplify their finances.</p>
+</form>
+<style>
+    #tsa{
+        color:#718fa2; 
+        font-size:155%;
+        font-family: Averta,sans-serif;
+    }
+    #input{
+            width:55%;
+            height: 7%;
+            margin: 1%;
+            border: 1px solid #b2c2cd;
+            border-radius: 4px;
+            font-family: Averta,sans-serif;
+            font-style: italic;
+            color: #b2c2cd;
+            font-size: 16px;
+
+       } 
+       #gh{
+           
+           margin-left: -36%;
+           color: #4d6575;
+           font-family: Averta,sans-serif;
+          font-size: 78%;
+
+
+       }
+       #vsm{
+           text-align: center;
+            width: 55%;
+            height: 7%;
+            color: black; 
+            font-size: 19px;
+            border: 1px solid #b2c2cd;
+            border-radius: 500px;
+            font-family: Averta,sans-serif;
+            margin-top: 40px;
+        } 
+    </style>
+<form>
+<div><input id="input" type="text" placeholder="  Email">
+        </div>
+        <div>
+            <input type="text" id="input" placeholder="  Password">
+        </div>
+    </center>
+    <center>
+
+<div id="gh">    At least 6 characters, but longer is better.</div>
+    </center>
+
+        
+     <center>   
+<button id="vsm"> Get started </button>
+</center>
+</form>
+    
+    </body>
+    </html>
