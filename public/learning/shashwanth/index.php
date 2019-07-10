1199f669f990eec42a4cd9836d2175405a64e771
shashwanth <perfectlyimperfect7688@gmail.com>
2019-07-10 11:46:23 +0530

    made into index

---
+++ public/learning/shashwanth/index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+</head>
+<body>
+<div style="padding:30px;">
+<a href="php data types.php">php data types</a><br>
+<a href="php home.php">php home</a><br>
+<a href="php constants.php">php constants</a><br>
+<a href="php echo.php">php echo</a><br>
+<a href="php else if else if.php">php else if else if</a><br>
+<a href="php operators.php">php operators</a><br>
+<a href="php strings.php">php strings</a><br>
+<a href="php variables.php">php variables</a><br>
+</div>
+</body>
+</html>
