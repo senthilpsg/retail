3e084e52b4995c06d82f90d1559172c6acfbe22b
MONISH-VELUSAMY <monish.vgp@gmail.com>
2019-07-16 21:53:25 +0530

    coustmer_statements

---
+++ public/coustmer_statements.php
@@ -0,0 +1,124 @@
+<html>
+
+<head></head>
+<style>
+#nkl {
+    border: 2px solid black;
+    height: 621px;
+    width: 1034px;
+    float: left;
+}
+
+#jk {
+    
+    height: 395px;
+    width: 1034px;
+
+}
+
+#btm {
+
+
+    border-radius: 8px;
+    border: 1px solid #b2c2cd;
+    height: 134px;
+    width: 773px;
+    margin-left: 134px;
+    margin-top: 49px;
+}
+
+#p2 {
+    float: right;
+    margin: 18px;
+    clear: right;
+    margin-right: 98px;
+    font-family: Averta, sans-serif;
+    color: #718fa2;
+    font-weight: normal;
+    font-size: 23px;
+    line-height: 1.4;
+}
+</style>
+
+<body>
+
+    <?php include 'menuvsm.php';?>
+    <div id="nkl">
+        <div id="jk">
+        
+            <p style="float:right;font-size:20px;margin:66px;margin-right: 125px;font-family: Averta,sans-serif;color: #7f309c;
+    font-weight: bold;">COMING SOON: CUSTOMER STATEMENTS</p>
+            <p style="float:right;margin:10px;clear:right;margin-right: 125px;color: solid black;line-height: 1.4;
+    font-weight: bold;font-size:31px;font-family: Averta,sans-serif;">Keep your customers in the<br> loop</p>
+            <p id="p2">Remind your customers about outstanding
+                <br> invoices or send details of their account<BR>activity.</p>
+
+
+        </div>
+
+        <div id="btm">
+
+            <p style="margin-top:39px; margin-left: 111px;font-family: Averta,sans-serif;"><strong>Get paid
+                    faster.</strong> When your customers know what they owe, you get paid.</p><br>
+            <p style="margin-top:-11px; margin-left: 111px;font-family: Averta,sans-serif;"><strong>Save
+                    time.</strong>Instead of resending invoices, show customers everything in one place.</p>
+        </div>
+
+
+
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
